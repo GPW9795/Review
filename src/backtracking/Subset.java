e0a5cd618d9fad876078d3aa9a86d0c0d367e13e
gaopeiwen <gaopeiwen.gpw@alibaba-inc.com>
2021-07-14 14:16:04 +0800

    sub set

---
+++ src/backtracking/Subset.java
@@ -0,0 +1,13 @@
+package backtracking;
+
+import java.util.*;
+
+/**
+ * 子集
+ * https://leetcode-cn.com/problems/subsets/
+ */
+public class Subset {
+    public List<List<Integer>> subsets(int[] nums) {
+        return null;
+    }
+}
