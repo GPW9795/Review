5b9d25ce7bba52c016197e4b79ba3d8bd92414b9
gaopeiwen <gaopeiwen.gpw@alibaba-inc.com>
2021-07-16 18:04:49 +0800

    sub set

--- src/backtracking/SubSet2.java
+++ src/backtracking/SubSet2.java
@@ -3,17 +3,18 @@ package backtracking;
 import java.util.*;
 
 /**
- * 子集
- * https://leetcode-cn.com/problems/subsets/
+ * 子集2
+ * https://leetcode-cn.com/problems/subsets-ii/
  */
-public class Subset {
-    private int len;
-    private List<List<Integer>> res = new ArrayList<>();
+public class SubSet2 {
+    public List<List<Integer>> res = new ArrayList<>();
     private List<Integer> list = new ArrayList<>();
+    private int len;
 
-    public List<List<Integer>> subsets(int[] nums) {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
         if (nums == null || nums.length == 0) return res;
         len = nums.length;
+        Arrays.sort(nums);
         dfs(nums, 0);
         return res;
     }
@@ -21,6 +22,7 @@ public class Subset {
     private void dfs(int[] nums, int begin) {
         res.add(new ArrayList<>(list));
         for (int i = begin; i < len; i++) {
+            if (i > begin && nums[i] == nums[i - 1]) continue;
             list.add(nums[i]);
             dfs(nums, i + 1);
             list.remove(list.size() - 1);
